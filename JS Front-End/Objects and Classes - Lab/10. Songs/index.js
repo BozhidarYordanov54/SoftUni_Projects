cc8955fd3a9e9e3591a8ed733de4972c8c3cbe2d
BozhidarYordanov54 <smvh@abv.bg>
2023-07-04 19:25:49 +0300

    Finished Lab: Objects and Classes

---
+++ JS Front-End/Objects and Classes - Lab/10. Songs/index.js
@@ -0,0 +1,39 @@
+function songs(songsArr)
+{
+    class Song
+    {
+        constructor(type, name, time)
+        {
+            this.type = type;
+            this.name = name;
+            this.time = time;
+        }
+    }
+
+    let songs = [];
+
+    let numOfSongs = songsArr[0];
+
+    for(let i = 1; i <= numOfSongs; i++)
+    {
+        let [type, name, time] = songsArr[i].split('_');
+        let song = new Song(type, name, time);
+        songs[i] = song;
+    }
+
+    let typeSongs = songsArr[songsArr.length - 1];
+
+    if(typeSongs === 'all')
+    {
+        songs.forEach((i) => console.log(i.name));
+    }
+    else
+    {
+        let filtered = songs.filter((i) => i.type === typeSongs);
+        filtered.forEach((i) => console.log(i.name));
+    }
+}
+
+//songs([3, 'favourite_DownTown_3:14', 'favourite_Kiss_4:16', 'favourite_Smooth Criminal_4:01', 'favourite']);
+//songs([4, 'favourite_DownTown_3:14', 'listenLater_Andalouse_3:24', 'favourite_In To The Night_3:58', 'favourite_Live It Up_3:48', 'listenLater']);
+songs([2, 'like_Replay_3:15', 'ban_Photoshop_3:48', 'all']);
