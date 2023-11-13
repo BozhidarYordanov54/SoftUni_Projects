a62a75f501b1158fd4fd89fc389f9c6a1cec39d5
BozhidarYordanov54 <smvh@abv.bg>
2023-11-13 18:05:48 +0200

    Update

---
+++ Entity Framewok - октомври 2023/Exercise_JSON Processing/CarDealer/CarDealer/Models/CategoryProduct.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class CategoryProduct
+    {
+        public int CategoryId { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+    }
+}
---
+++ Entity Framewok - октомври 2023/Exercise_JSON Processing/CarDealer/CarDealer/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+
+        public int SellerId { get; set; }
+        [ForeignKey(nameof(User.Id))]
+
+
+        public int BuyerId { get; set; }
+        [ForeignKey(nameof(User.Id))]
+
+
+    }
+}
